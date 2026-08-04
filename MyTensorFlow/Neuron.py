4e8600824cf762699f8662d7bceed1a49e31cc69
povvithya viseth <povvithyaviseth@csus.edu>
2026-08-04 11:25:43 -0700

    test: Neuron Class
    Constructor, Forward Function

--- MyTensorFlow/Neuron.py
+++ MyTensorFlow/Neuron.py
@@ -1,10 +1,9 @@
-import numpy as np
 from MyTensorFlow.ActivationFunction import *
 
 
 class Neuron:
     def __init__(self, size=0 ,activation=linear):
-        self.w = np.zeros(size)
+        self.w = np.random.randn(size)
         self.b = 0.0
         self.activation = activation
 
