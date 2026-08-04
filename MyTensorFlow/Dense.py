57c8eeb6ecf54a11e7a423864672002a9a73dfcf
povvithya viseth <povvithyaviseth@csus.edu>
2026-08-04 12:18:03 -0700

    refactor: Give w and b some Value

--- MyTensorFlow/Dense.py
+++ MyTensorFlow/Dense.py
@@ -6,7 +6,9 @@ from MyTensorFlow.Neuron import Neuron
 
 class Dense:
     def __init__(self, units=0, activation=linear):
-        self._neurons = [Neuron(3,activation) for _ in range(units)]
+        self._units = units
+        self._activation = activation
 
     def forward(self, x):
-        return np.array([neuron.forward(x) for neuron in self._neurons])
+        neurons = [Neuron(x.size,self._activation) for _ in range(self._units)]
+        return np.array([neuron.forward(x) for neuron in neurons])
