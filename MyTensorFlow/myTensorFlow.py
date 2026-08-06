9aefa3a35e6e8595ee602cddf701d35ab8fc96d5
povvithya viseth <povvithyaviseth@csus.edu>
2026-08-06 16:20:29 -0700

    feat: Fit Function

--- MyTensorFlow/myTensorFlow.py
+++ MyTensorFlow/myTensorFlow.py
@@ -10,7 +10,21 @@ w (ndarray (n, )): n Feature
 b (scalar): Bias
 """
 
-def compute_gradient(X, y, w, b, activation=linear):
+def fit(X, y, epochs=1000, learning_rate=0.01, activation=linear):
+    m, n = X.shape
+
+    w = np.zeros(n)
+    b = 0.0
+
+    for epoch in range(epochs):
+        dj_dw, dj_db = compute_gradient(X, y, w, b, activation)
+        for j in range(n):
+            w[j] += learning_rate * dj_dw[j]
+        b += learning_rate * dj_db
+
+    return w,b
+
+def compute_gradient(X, y, w, b, function):
     m, n = X.shape
 
     dj_dw = np.zeros(n)
@@ -18,8 +32,8 @@ def compute_gradient(X, y, w, b, activation=linear):
 
     for i in range(m):
         f_wb_i = affine(X[i], w, b)
-        predict = activation(f_wb_i)
-        err_i = predict - [i]
+        predict = function(f_wb_i)
+        err_i = predict - y[i]
         for j in range(n):
             dj_dw[j] += err_i * X[i][j]
         dj_db += err_i
