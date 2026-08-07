b0c80471d7a5e8ef3ec7d0a4538b5888414d01ad
povvithya viseth <povvithyaviseth@csus.edu>
2026-08-07 11:59:06 -0700

    refactor: Move logistic_cost_function Out

--- MyTensorFlow/myTensorFlow.py
+++ MyTensorFlow/myTensorFlow.py
@@ -12,7 +12,6 @@ b (scalar): Bias
 
 def fit(X, y, epochs=1000, learning_rate=0.01, activation=linear):
     m, n = X.shape
-
     w = np.zeros(n)
     b = 0.0
     for epoch in range(epochs):
@@ -22,21 +21,10 @@ def fit(X, y, epochs=1000, learning_rate=0.01, activation=linear):
         b -= learning_rate * dj_db
     return w,b
 
-def logistic_cost_function(X, y, w, b):
-    m, n = X.shape
-    cost = 0.0
-    for i in range(m):
-        z = affine(X[i], w, b)
-        predict = sigmoid(z)
-        cost += y[i] * np.log(predict) + (1-y[i]) * np.log(1-predict)
-    return -1/m * cost
-
 def compute_gradient(X, y, w, b, function):
     m, n = X.shape
-
     dj_dw = np.zeros(n)
     dj_db = 0.0
-
     for i in range(m):
         z = affine(X[i], w, b)
         predict = function(z)
@@ -44,9 +32,6 @@ def compute_gradient(X, y, w, b, function):
         for j in range(n):
             dj_dw[j] += err_i * X[i][j]
         dj_db += err_i
-
-
     dj_dw /= m
     dj_db /= m
-
     return dj_dw, dj_db
