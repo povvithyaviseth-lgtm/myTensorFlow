8488960e8f988d48d65d1d6a6a5ca2dba5e258ce
povvithya viseth <povvithyaviseth@gmail.com>
2026-08-09 10:29:06 -0700

    refactor: Rename One of the Parameter

--- Dense.py
+++ Dense.py
@@ -5,10 +5,13 @@ from Neuron import Neuron
 from ActivationFunction import linear
 
 class Dense:
-    def __init__(self, num_feature=0, units=0, activation=linear, loss=MSE):
-        self._neurons = [Neuron(num_feat=num_feature,activation=activation,loss=loss)
+    def __init__(self, input_size=0, units=0, activation=linear, loss=MSE):
+        self._neurons = [Neuron(input_size=input_size,activation=activation,loss=loss)
                          for _ in range(units)]
 
     def forward(self, X):
         output = [neuron.forward(X) for neuron in self._neurons]
         return np.array(output)
+
+    def get_neurons(self):
+        return self._neurons
