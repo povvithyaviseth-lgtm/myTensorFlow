03e617e4bd3dbbfd6222045fc99ba6a5862b12df
povvithya viseth <povvithyaviseth@gmail.com>
2026-08-09 17:35:56 -0700

    feat: Compile(loss)

--- Sequential.py
+++ Sequential.py
@@ -1,4 +1,4 @@
-from LossFunction import BinaryCrossentropy, dJ_da
+from LossFunction import dJ_da, MSE
 
 """
 layers (List of Dense)
@@ -6,6 +6,7 @@ layers (List of Dense)
 class Sequential:
     def __init__(self, layers):
         self._layers = layers
+        self._loss = MSE
 
     """
     return a List of Activation Result
@@ -16,8 +17,11 @@ class Sequential:
             a = layer.forward(a)
         return a
 
+    def compile(self,loss=MSE):
+        self._loss=loss
+
     def fit(self,X, y, epochs=100):
-        dJ_da_func = dJ_da(loss=BinaryCrossentropy)
+        dJ_da_func = dJ_da(loss=self._loss)
         for epoch in range(epochs):
             total_cost = 0
             for i in range(len(X)):
@@ -25,7 +29,7 @@ class Sequential:
                 target = y[i]
 
                 a = self.forward(x)
-                total_cost += BinaryCrossentropy(a,target) # Change this Later
+                total_cost += self._loss(a,target) # Change this Later
 
                 dJ_da_i = dJ_da_func(a, target)
                 gradients = []
